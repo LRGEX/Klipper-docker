72a36686643a89260158083988e54b9b4596fadc
lrgex <info@lrgex.com>
2023-11-13 21:31:30 +0300

    v1.5.1

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -11,6 +11,8 @@ cat << "EOF"
 ╩ ╩┴─┘┴┴  ┴  └─┘┴└─   v1.5.1                                  
 EOF
 
+##################### Starting code #####################
+
 # This script will run on every container start
 if ! systemctl is-enabled nginx > /dev/null 2>&1; then
     echo "Enabling NGINX..."
@@ -23,8 +25,19 @@ else
     echo "NGINX is already running."
 fi
 
-# Start systemd /lib/systemd/systemd this for systemd to initialize, "log-level=info" is for debugging , "unit=sysinit.target" is for systemd to initialize
+##################### End code #####################
+
+
+
+
+##################### Only for systemd #####################
+
+# Start systemd /lib/systemd/systemd this for systemd to initialize, "log-level=info" is for debugging , "unit=sysinit.target" is for systemd to initialize AS IS for running the container with systemd
 exec /lib/systemd/systemd log-level=info unit=sysinit.target
+
+##################### End Only for systemd #####################
+
+
 
 exec "$@" & wait # This is needed to run other scripts or commands when running the container like docker run -it --rm backarosa:latest /bin/bash, backup,restore,container_start,container_stop ..etc
 #so when you run the container it will run only one command from scripts folder and exit, however commands in line 3 and 4 will run on every docker run - mandatory hard coded - 
